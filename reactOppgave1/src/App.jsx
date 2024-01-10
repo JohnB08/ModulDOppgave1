5197e825bccbf5f573b17e1435aabdcdc976bc4c
JohnB08 <142892183+JohnB08@users.noreply.github.com>
2024-01-10 17:17:47 +0100

    Starting "navbar" design

--- reactOppgave1/src/App.jsx
+++ reactOppgave1/src/App.jsx
@@ -2,6 +2,15 @@ import imageData from "./Data/adjustedImageData.json";
 import "./App.css";
 import MakeImageContainer from "./components/ImageContainer/ImageContainer.jsx";
 import NavBar from "./components/NavBar/NavBar";
+
+function Footer() {
+  return (
+    <footer>
+      <p>This is a p tag inside a footer element.</p>
+    </footer>
+  );
+}
+
 function App() {
   console.log(imageData);
   console.log(Array.isArray(imageData));
@@ -9,6 +18,7 @@ function App() {
     <>
       <NavBar />
       <MakeImageContainer array={imageData} />
+      <Footer />
     </>
   );
 }
