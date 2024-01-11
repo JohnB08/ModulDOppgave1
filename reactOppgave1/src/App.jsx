ec4394ae940c5ffa8357340e46b64725ff972200
JohnB08 <142892183+JohnB08@users.noreply.github.com>
2024-01-11 10:57:00 +0100

    ignore error, error is wrong

--- reactOppgave1/src/App.jsx
+++ reactOppgave1/src/App.jsx
@@ -2,7 +2,7 @@ import imageData from "./Data/adjustedImageData.json";
 import "./App.css";
 import MakeImageContainer from "./components/ImageContainer/ImageContainer.jsx";
 import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/footer";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   console.log(imageData);
