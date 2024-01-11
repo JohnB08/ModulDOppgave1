b02ec305a4d3a5851565b0088ec3f3110a30dd23
JohnB08 <142892183+JohnB08@users.noreply.github.com>
2024-01-11 11:14:48 +0100

    comments

--- reactOppgave1/src/App.jsx
+++ reactOppgave1/src/App.jsx
@@ -2,6 +2,7 @@ import imageData from "./Data/adjustedImageData.json";
 import "./App.css";
 import MakeImageContainer from "./components/ImageContainer/ImageContainer.jsx";
 import NavBar from "./components/NavBar/NavBar";
+//!! Denne prøver å autocomplete til Footer/footer som gir en error under build, men Footer/Footer gir feil i "strict" mode?
 import Footer from "./components/Footer/Footer";
 
 function App() {
